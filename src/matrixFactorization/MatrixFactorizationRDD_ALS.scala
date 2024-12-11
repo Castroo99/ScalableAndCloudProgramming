3e93845cdead5f5fae116a1677d956a5baeed012
Anna Tosoroni <anna.tosoroni@gmail.com>
2024-12-11 16:37:24 +0100

    fixed dataframe input matrix factorization

--- src/matrixFactorization/MatrixFactorizationRDD_ALS.scala
+++ src/matrixFactorization/MatrixFactorizationRDD_ALS.scala
@@ -1,6 +1,6 @@
 package MatrixFactorizationModule
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.{SparkConf, SparkContext} 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.recommendation.{ALS, Rating}
@@ -12,6 +12,7 @@ import kantan.csv._
 import kantan.csv.ops._
 import scala.math.BigDecimal.RoundingMode
 
+
 object MatrixFactorizationRDD_ALS {
   //❌💪
   def main(args: Array[String]): Unit = {
@@ -22,9 +23,8 @@ object MatrixFactorizationRDD_ALS {
 
     val userId_selected = args(0).toInt
     val numMoviesRec = args(1).toInt
-    val sentimentFile = args(2)
+    val sentimentDF = args(2)
     val outputFile = args(3)
-
 
     print("Starting MatrixFactorizationRDD_ALS")
 
@@ -34,25 +34,29 @@ object MatrixFactorizationRDD_ALS {
       .appName("ReccSys")
       .master("local[4]") // 4 thread
       .getOrCreate()
-          
-    val rawRdd: RDD[String] = spark.sparkContext.textFile(sentimentFile)
     
-    // header rimosso da RDD
-    val header = rawRdd.first()
-    val dataRdd: RDD[String] = rawRdd.filter(line => line != header)
+    import spark.implicits._
+
+          
+    // val rawRdd: RDD[String] = spark.sparkContext.textFile(sentimentFile)
+    
+    // // header rimosso da RDD
+    // val header = rawRdd.first()
+    // val dataRdd: RDD[String] = rawRdd.filter(line => line != header)
     
     // mappa ogni riga del csv in un oggetto Rating con userId, movieId e totalScore
-    val ratingsRdd: RDD[Rating] = dataRdd.map { line =>
-     /*  val fields = line.split(",")
-      val userId = fields(3).toInt
-      val movieId = fields(4).toInt
-      val rating = fields(0).toDouble
-      val sentimentResult = fields(2).toDouble */
-      val fields = line.split(",")
-      val userId = fields(3).toInt
-      val movieId = fields(0).toInt
-      val rating = fields(1).toDouble
-      val sentimentResult = fields(4).toDouble
+    val ratingsRdd: RDD[Rating] = sentimentDF.rdd.map { 
+      row =>
+      val userId = row.getAs[Int]("userId")
+      val movieId = row.getAs[Int]("movieId")
+      val rating = row.getAs[Double]("rating")
+      val sentimentResult = row.getAs[Double]("sentimentResult")
+      //line =>
+      // val fields = line.split(",")
+      // val userId = fields(3).toInt
+      // val movieId = fields(0).toInt
+      // val rating = fields(1).toDouble
+      // val sentimentResult = fields(4).toDouble
       val totalScore = (rating * 0.5) + (sentimentResult * 0.5)
       Rating(userId, movieId, totalScore)
     }//.repartition(numPartitions)
@@ -105,6 +109,8 @@ object MatrixFactorizationRDD_ALS {
     // Convert recommendations to CSV format in memory
     val csvData = new ByteArrayOutputStream()
     val writer = CSVWriter.open(csvData)
+
+    // val writer = CSVWriter.open(new java.io.File(outputPath))
 
     // Scrive header
     writer.writeRow(Seq("userId", "movieId", "totalScore"))
